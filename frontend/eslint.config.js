514f88522fb158d1f999a2c311474f3779c9b81e
bifshteksex <roman_shangin@vk.com>
2026-01-24 18:00:45 +0200

    CI fix #5 | phase 0

---
+++ frontend/eslint.config.js
@@ -0,0 +1,66 @@
+import js from '@eslint/js';
+import tsParser from '@typescript-eslint/parser';
+import tsPlugin from '@typescript-eslint/eslint-plugin';
+import sveltePlugin from 'eslint-plugin-svelte';
+import svelteParser from 'svelte-eslint-parser';
+import prettier from 'eslint-config-prettier';
+import globals from 'globals';
+
+export default [
+	js.configs.recommended,
+	{
+		ignores: [
+			'.svelte-kit/**',
+			'build/**',
+			'dist/**',
+			'node_modules/**',
+			'**/*.min.js',
+			'vite.config.ts.timestamp-*'
+		]
+	},
+	{
+		files: ['**/*.js', '**/*.ts'],
+		languageOptions: {
+			parser: tsParser,
+			parserOptions: {
+				ecmaVersion: 'latest',
+				sourceType: 'module'
+			},
+			globals: {
+				...globals.browser,
+				...globals.node,
+				...globals.es2021
+			}
+		},
+		plugins: {
+			'@typescript-eslint': tsPlugin
+		},
+		rules: {
+			...tsPlugin.configs.recommended.rules,
+			'@typescript-eslint/no-unused-vars': [
+				'warn',
+				{ argsIgnorePattern: '^_', varsIgnorePattern: '^_' }
+			]
+		}
+	},
+	{
+		files: ['**/*.svelte'],
+		languageOptions: {
+			parser: svelteParser,
+			parserOptions: {
+				parser: tsParser,
+				extraFileExtensions: ['.svelte']
+			},
+			globals: {
+				...globals.browser
+			}
+		},
+		plugins: {
+			svelte: sveltePlugin
+		},
+		rules: {
+			...sveltePlugin.configs.recommended.rules
+		}
+	},
+	prettier
+];
