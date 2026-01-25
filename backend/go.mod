2285511b04f165807b6c70ce380f04c0d89227d9
bifshteksex <roman_shangin@vk.com>
2026-01-25 14:10:10 +0200

    fix(go): update versions

--- backend/go.mod
+++ backend/go.mod
@@ -8,7 +8,7 @@ require (
 	github.com/google/uuid v1.6.0
 	github.com/jackc/pgx/v5 v5.8.0
 	github.com/nats-io/nats.go v1.48.0
-	github.com/redis/go-redis/v9 v9.17.2
+	github.com/redis/go-redis/v9 v9.17.3
 	golang.org/x/crypto v0.47.0
 	golang.org/x/oauth2 v0.34.0
 	gopkg.in/yaml.v3 v3.0.1
--- backend/update-deps.bat
+++ backend/update-deps.bat
@@ -1,6 +1,5 @@
 @echo off
 chcp 65001 >nul
-setlocal enabledelayedexpansion
 
 echo Checking current Go version...
 go version
@@ -8,32 +7,15 @@ go version
 echo.
 echo Updating all direct dependencies to stable versions...
 
-REM Get list of direct dependencies
-go list -m -f "{{if not .Indirect}}{{.Path}}{{end}}" all > temp_deps.txt
-
-REM Update each dependency
-for /f "usebackq tokens=*" %%m in ("temp_deps.txt") do (
-    if not "%%m"=="" (
-        if not "%%m"=="github.com/bifshteksex/hertzboard" (
-            echo   Updating %%m@latest (stable)...
-            go get "%%m@latest"
-        )
-    )
-)
-
-REM Clean up temp file
-del temp_deps.txt
-
-echo.
-echo Updating indirect dependencies (patch level)...
-go get -u=patch ./...
+REM Use go get to update all dependencies
+go get -u ./...
 
 echo.
 echo Cleaning up and optimizing go.mod...
 go mod tidy
 
 echo.
-echo All dependencies updated to stable versions!
+echo All dependencies updated!
 echo.
 echo Checking updated dependencies:
 go list -m -u all
